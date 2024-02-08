618eb7d870e570ffbd1337c27d234566b3fa1b17
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2024-02-08 05:03:30 -0300

    💡 Added docstrings

--- extracao/datasources/connectors.py
+++ extracao/datasources/connectors.py
@@ -12,50 +12,57 @@ from pymongo import MongoClient
 
 # from motor.motor_asyncio import AsyncIOMotorClient
 
+
 # %% ../../nbs/01a_connectors.ipynb 5
 @dataclass
 class DBConnector:
-    def connect(self):
-        raise NotImplementedError
+	"""Base class for database connectors"""
 
-    def get_parallel_connections(self, n):
-        # This method returns a list of n pyodbc connection objects in parallel
-        with ThreadPoolExecutor(max_workers=n) as executor:
-            futures = [executor.submit(self.connect) for _ in range(n)]
-            return [future.result() for future in futures]
+	def connect(self):
+		raise NotImplementedError
+
+	def get_parallel_connections(self, n):
+		# This method returns a list of n pyodbc connection objects in parallel
+		with ThreadPoolExecutor(max_workers=n) as executor:
+			futures = [executor.submit(self.connect) for _ in range(n)]
+			return [future.result() for future in futures]
+
 
 # %% ../../nbs/01a_connectors.ipynb 7
 @dataclass
 class SQLServer(DBConnector):
-    sql_params: dict
+	"""Class for connecting to SQL Server"""
 
-    def connect(self):
-        # This method returns a pyodbc connection object according to the platform
-        connection_string = f"""Driver={self.sql_params.get('driver')};
-                                Server={self.sql_params.get('server')};
-                                Database={self.sql_params.get('database')};
-                                Encrypt=no;"""
-        if self.sql_params.get("trusted_conn"):
-            connection_string += f"""MultipleActiveResultSets={self.sql_params.get('mult_results')};
-                                  Trusted_Connection=yes;"""
-        else:
-            connection_string += f"""UID={self.sql_params.get('username')};
-                                  PWD={self.sql_params.get('password')};
-                                  Trusted_Connection=no;"""
-        try:
-            return pyodbc.connect(
-                connection_string, timeout=self.sql_params.get("timeout", 10000)
-            )
-        except pyodbc.OperationalError as e:
-            raise ConnectionError(
-                "Não foi possível abrir uma conexão com o SQL Server. Esta conexão somente funciona da rede cabeada!"
-            ) from e
+	sql_params: dict
+
+	def connect(self):
+		"""This method returns a pyodbc connection object according to the os platform"""
+		connection_string = f"""Driver={self.sql_params.get('driver')};
+								Server={self.sql_params.get('server')};
+								Database={self.sql_params.get('database')};
+								Encrypt=no;"""
+		if self.sql_params.get('trusted_conn'):
+			connection_string += f"""MultipleActiveResultSets={self.sql_params.get('mult_results')};
+								  Trusted_Connection=yes;"""
+		else:
+			connection_string += f"""UID={self.sql_params.get('username')};
+								  PWD={self.sql_params.get('password')};
+								  Trusted_Connection=no;"""
+		try:
+			return pyodbc.connect(connection_string, timeout=self.sql_params.get('timeout', 10000))
+		except pyodbc.OperationalError as e:
+			raise ConnectionError(
+				'Não foi possível abrir uma conexão com o SQL Server. Esta conexão somente funciona da rede cabeada!'
+			) from e
+
 
 # %% ../../nbs/01a_connectors.ipynb 9
 @dataclass
 class MongoDB(DBConnector):
-    mongo_uri: str
+	"""Class for connecting to MongoDB"""
 
-    def connect(self):
-        # This method returns a connected AsyncIOMotorClient object
-        return MongoClient(self.mongo_uri)
+	mongo_uri: str
+
+	def connect(self):
+		"""This method returns a connected MongoClient object"""
+		return MongoClient(self.mongo_uri)
