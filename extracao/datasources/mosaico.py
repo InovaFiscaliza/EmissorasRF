29f6b03b6ad4b30b9d770421004195ecd7417029
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2024-02-20 00:57:32 -0300

    🐛 Cast df to string before calling general regex expression

--- extracao/datasources/mosaico.py
+++ extracao/datasources/mosaico.py
@@ -44,9 +44,11 @@ class Mosaico(Base, GetAttr):
 		client = self.connect()
 		database = client[self.database]
 		db_collection = database[collection]
-		df = pd.DataFrame(list(db_collection.aggregate(pipeline)), copy=False)
+		df = pd.DataFrame(list(db_collection.aggregate(pipeline)), copy=False).astype(
+			'string', copy=False
+		)
 		# Substitui strings vazias e somente com espaços por nulo
-		return df.replace(r'^\s*$|^\[\]$', pd.NA, regex=True).astype('string', copy=False)
+		return df.replace(r'^\s*$|^\[\]$', pd.NA, regex=True)
 
 	def split_designacao(
 		self,
