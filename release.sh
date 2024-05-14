49a592888b4a4903b0706d525d94447e10dca4a3
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2024-05-14 04:49:21 -0300

    Created release script

---
+++ release.sh
@@ -0,0 +1,3 @@
+gh release create v2.0.1 --generate-notes .\extracao\datasources\arquivos\saida\estacoes.parquet.gzip .\extracao\datasources\arquivos\saida\log.parquet.gzip
+gh release delete rfdatahub --cleanup-tag -R InovaFiscaliza/.github -y
+gh release create rfdatahub -t RFDataHub --notes "v2.0.1" .\extracao\datasources\arquivos\saida\estacoes.parquet.gzip .\extracao\datasources\arquivos\saida\log.parquet.gzip .\extracao\datasources\arquivos\saida\Release.json -R InovaFiscaliza/.github
